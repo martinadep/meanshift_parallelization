9e180ac134a73af414e2fc413525530bd0beaa45
martinadep <depiccolimartina26@gmail.com>
2025-04-11 00:35:34 +0200

    changed abs path

--- py_utils/config.py
+++ py_utils/config.py
@@ -1,4 +1,4 @@
-in_img_path = "/examples/sample_astro.jpg"
-original_csv_path = "/data/original.csv"
-modified_csv_path = "/data/modified.csv"
-out_img_path = "/data/reconstructed.jpg"
+in_img_path = "./examples/sample_astro.jpg"
+original_csv_path = "./data/original.csv"
+modified_csv_path = "./data/modified.csv"
+out_img_path = "./data/reconstructed.jpg"
